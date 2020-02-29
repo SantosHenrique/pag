f5d61a12271de5220a56b474edf116033042f559
Henrique Fernandes Santos <fernandeshenrique@outlook.com>
2020-02-29 09:36:00 -0300

    Entidade aluno

---
+++ Dominio/Interfaces/IEntidadeIdentidade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio.Interfaces
+{
+    public interface IEntidadeIdentidade
+    {
+        int Id { get; set; }
+        DateTime Insercao { get; set; }
+        DateTime Atualizacao { get; set; }
+    }
+}
